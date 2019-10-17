45b88f4784ca2cdeda1001af37b9a27c36e0b39a
Daniel Chalef <daniel.chalef@sparkpost.com>
2019-10-16 19:17:38 -0700

    backport of [AIRFLOW-5117]

--- airflow/contrib/kubernetes/kube_client.py
+++ airflow/contrib/kubernetes/kube_client.py
@@ -14,12 +14,19 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+from typing import Optional
 from airflow.configuration import conf
 from six import PY2
 
 try:
     from kubernetes import config, client
     from kubernetes.client.rest import ApiException
+    from kubernetes.client.api_client import ApiClient
+    from kubernetes.client import Configuration
+    from airflow.kubernetes.refresh_config import (  # pylint: disable=ungrouped-imports
+        load_kube_config,
+        RefreshConfiguration,
+    )
     has_kubernetes = True
 except ImportError as e:
     # We need an exception class to be able to use it in ``except`` elsewhere
@@ -29,28 +36,62 @@ except ImportError as e:
     _import_err = e
 
 
-def _load_kube_config(in_cluster, cluster_context, config_file):
+def _get_kube_config(in_cluster: bool,
+                        cluster_context: Optional[str],
+                        config_file: Optional[str]) -> Optional[Configuration]:
+    if in_cluster:
+        # load_incluster_config set default configuration with config populated by k8s
+        config.load_incluster_config()
+        return None
+    else:
+        # this block can be replaced with just config.load_kube_config once
+        # refresh_config module is replaced with upstream fix
+        cfg = RefreshConfiguration()
+        load_kube_config(
+            client_configuration=cfg, config_file=config_file, context=cluster_context)
+        return cfg
+
+
+def _get_client_with_patched_configuration(cfg: Optional[Configuration]) -> client.CoreV1Api:
+    '''
+    This is a workaround for supporting api token refresh in k8s client.
+    The function can be replace with `return client.CoreV1Api()` once the
+    upstream client supports token refresh.
+    '''
+    if cfg:
+        return client.CoreV1Api(api_client=ApiClient(configuration=cfg))
+    else:
+        return client.CoreV1Api()
+
+
+# def _load_kube_config(in_cluster, cluster_context, config_file):
+#     if not has_kubernetes:
+#         raise _import_err
+#     if in_cluster:
+#         config.load_incluster_config()
+#     else:
+#         config.load_kube_config(config_file=config_file, context=cluster_context)
+#     if PY2:
+#         # For connect_get_namespaced_pod_exec
+#         from kubernetes.client import Configuration
+#         configuration = Configuration()
+#         configuration.assert_hostname = False
+#         Configuration.set_default(configuration)
+#     return client.CoreV1Api()
+
+
+def get_kube_client(in_cluster: bool = conf.getboolean('kubernetes', 'in_cluster'),
+                    cluster_context: Optional[str] = None,
+                    config_file: Optional[str] = None):
+    
     if not has_kubernetes:
         raise _import_err
-    if in_cluster:
-        config.load_incluster_config()
-    else:
-        config.load_kube_config(config_file=config_file, context=cluster_context)
-    if PY2:
-        # For connect_get_namespaced_pod_exec
-        from kubernetes.client import Configuration
-        configuration = Configuration()
-        configuration.assert_hostname = False
-        Configuration.set_default(configuration)
-    return client.CoreV1Api()
 
-
-def get_kube_client(in_cluster=conf.getboolean('kubernetes', 'in_cluster'),
-                    cluster_context=None,
-                    config_file=None):
     if not in_cluster:
         if cluster_context is None:
             cluster_context = conf.get('kubernetes', 'cluster_context', fallback=None)
         if config_file is None:
             config_file = conf.get('kubernetes', 'config_file', fallback=None)
-    return _load_kube_config(in_cluster, cluster_context, config_file)
+            
+    client_conf = _get_kube_config(in_cluster, cluster_context, config_file)
+    return _get_client_with_patched_configuration(client_conf)
